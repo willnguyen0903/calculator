154ea0fc0ff418c223bcd6641aa6ecab897413b7
willnguyen0903 <trnguyen0903@gmail.com>
2023-12-28 17:21:45 -0500

    version 1.1

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
 const displayScreen = document.querySelector('#display-screen');
+const resultScreen = document.querySelector('#result-screen');
 //append numbers, operators and decimal
 function appendNumber(number) {
   displayScreen.textContent += number;
 }
 function appendOperator(operator) {
-  displayScreen.textContent += operator;
+  if(result) {
+    displayScreen.textContent = result + operator;
+  } else {
+  displayScreen.textContent += operator; 
+  }
 }
 function appendDecimal(decimal) {
   displayScreen.textContent += decimal;
- 
 }
-
+let result = null;
 function calculateResult() {
   result = calculateExpression(displayScreen.textContent);
-  displayScreen.textContent = result;
+  resultScreen.textContent = result;
 }
 //fuction to evaluate the expression 
-function calculateResult() {
-  const expression = displayScreen.textContent;
+function calculateExpression(expression) {
+  // Validate the expression to ensure it contains only allowed characters
+  const validCharsRegex = /^[0-9+\-*/(). ]*$/;
+  if (!validCharsRegex.test(expression)) {
+      throw new Error('Invalid characters in expression');
+  }
 
+  // Use try-catch to handle potential errors during evaluation
   try {
-    // Use eval to calculate the result (replace with your own calculation logic)
-    const result = eval(expression);
+      // Use the Function constructor to create a function and pass the expression as an argument
+      const evalFunction = new Function('return ' + expression);
 
-    // Check for division by 0
-    if (result === Infinity) {
-      alert("Error: Division by 0!");
-      clearDisplay();
-    } else {
-      displayScreen.textContent = result;
-    }
+      // Call the function to evaluate the expression
+      const result = evalFunction();
+      // Check for NaN or Infinity results
+      if (!isFinite(result)) {
+          alert('Invalid result!');
+          throw new Error('Invalid result');
+      }
+
+      return result;
   } catch (error) {
-    alert("Error: Invalid expression");
-    clearDisplay();
+      alert('Invalid expression!');
+      clearDisplay();
+      throw new Error('Error evaluating expression: ' + error.message);
   }
 }
 
 function clearDisplay() {
   displayScreen.textContent = '';
+  resultScreen.textContent = '0';
+  result = null;
 }
 //toggle sign 
 let signToggled = false;
@@ -52,12 +66,13 @@ function toggleSign() {
 
 //percentage calculation
 function calculatePercentage() {
-  displayScreen.textContent = parseFloat(displayScreen.textContent)/100;
+  resultScreen.textContent = parseFloat(calculateExpression
+                              (displayScreen.textContent)/100);
+  displayScreen.textContent = resultScreen.textContent;                              
 }
-
+//eventlistener for keyboard inputs
 document.addEventListener('keydown', function (event) {
   const key = event.key.toLowerCase();
-
   if (key === 'enter') {
     document.getElementById('btn=').click();
   } else if (key === 'backspace') {
@@ -66,6 +81,8 @@ document.addEventListener('keydown', function (event) {
     const displayContent = document.getElementById('display-screen');
     const currentText = displayContent.textContent;
     displayContent.textContent = currentText.slice(0, -1);
+  } else if (key === '*') {
+    document.getElementById('btnx').click();
   } else {
     const button = document.getElementById(`btn${key}`);
     if (button) {
@@ -73,6 +90,9 @@ document.addEventListener('keydown', function (event) {
     }
   }
 });
+
+//after a calculation is made, 
+//another input will assign the previous result to the display screen
 
 
 
